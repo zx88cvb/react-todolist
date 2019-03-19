f2cabb0ed633c86508d0928ed47e9277c78514b2
Angel <295389754@qq.com>
2019-03-19 14:57:34 +0800

    redux

--- src/TodoListAnt.js
+++ src/TodoListAnt.js
@@ -7,23 +7,58 @@ class TodoListAnt extends Component {
   constructor(props) {
     super(props);
     this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    // 订阅store store改变
+    store.subscribe(this.handleStoreChange);
   }
 
   render() {
     return (
       <div style={{marginTop: '10px', marginLeft: '10px'}}>
         <div>
-          <Input value={this.state.inputValue} placeholder="todo info" style={{width: '300px', marginRight: '10px'}}/>
-          <Button type="primary">提交</Button>
+          <Input value={this.state.inputValue}
+          placeholder="todo info"
+          style={{width: '300px', marginRight: '10px'}}
+          onChange={this.handleInputChange}/>
+          <Button type="primary" onClick={this.handleButtonClick}>提交</Button>
         </div>
         <List
           style={{marginTop: '10px', width: '300px'}}
           bordered
           dataSource={this.state.list}
-          renderItem={item => (<List.Item>{item}</List.Item>)}
+          renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
         />
       </div>
     )
+  }
+
+  handleInputChange(e) {
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
+    store.dispatch(action);
+  }
+
+  handleStoreChange () {
+    this.setState(store.getState());
+  }
+
+  handleButtonClick () {
+    const action = {
+      type: 'add_todo_item'
+    }
+    store.dispatch(action);
+  }
+
+  handleItemDelete (index) {
+    const action = {
+      type: 'delete_todo_item',
+      index
+    }
+    store.dispatch(action);
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,28 @@
 const defaultState = {
   inputValue: '',
-  list: [1, 2]
+  list: []
 }
 
+// reducer 可以接受state 但是绝对不能修改state
 export default (state = defaultState, action) => {
+  if (action.type === 'change_input_value') {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.inputValue = action.value;
+    return newState;
+  }
+
+  if (action.type === 'add_todo_item') {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.push(newState.inputValue);
+    newState.inputValue = '';
+    return newState;
+  }
+
+  if (action.type === 'delete_todo_item') {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.splice(action.index, 1)
+    return newState;
+  }
+
   return state;
 }
